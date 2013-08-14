59f82229bf1a4711c0c4d1c46aad67b94e5ac296
Michael Park <migrap@gmail.com>
2013-08-14 09:13:22 -0700

    Added a playground

---
+++ src/Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Travitor;
+
+namespace Playground {
+    class Program {
+        static void Main(string[] args) {
+            var travitor = TravitorClient.New();
+            travitor.LoginAsync("michael.park", "!!urDead").Wait();
+
+            travitor.GetApiAsync();
+
+            (new AutoResetEvent(false)).WaitOne();
+        }
+    }
+}
